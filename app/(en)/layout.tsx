11c51360a8c114fc18fa10b748f5482e0a5c2d8a
Mohammed Ehab <75863185+m7mdehab@users.noreply.github.com>
2026-09-12 01:13:33 +0300

    Load refinement safeguards after primary Iteration 13 layer

--- app/(en)/layout.tsx
+++ app/(en)/layout.tsx
@@ -4,6 +4,7 @@ import "@fontsource-variable/newsreader";
 import "../globals.css";
 import "../hardening.css";
 import "../refinement.css";
+import "../refinement-fixes.css";
 import { SiteNav } from "@/components/site-nav";
 import { SmoothScroll } from "@/components/smooth-scroll";
 import { projectRecords, serviceRecords } from "@/data/discoverability";
