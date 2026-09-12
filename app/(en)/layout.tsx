6b8a32f55ebfc21e273e2df8d8528d76916513ac
Mohammed Ehab <75863185+m7mdehab@users.noreply.github.com>
2026-09-12 04:23:11 +0300

    fix: load Phase D rendered-QA corrections

--- app/(en)/layout.tsx
+++ app/(en)/layout.tsx
@@ -6,6 +6,7 @@ import "../hardening.css";
 import "../refinement.css";
 import "../refinement-fixes.css";
 import "../frontend-overhaul.css";
+import "../frontend-overhaul-fixes.css";
 import { SiteNav } from "@/components/site-nav";
 import { SmoothScroll } from "@/components/smooth-scroll";
 import { projectRecords, serviceRecords } from "@/data/discoverability";
